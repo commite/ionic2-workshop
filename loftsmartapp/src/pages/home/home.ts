b91ea498b650bd5c2dd0df2fd2407f3da01f5b9d
msaelices <msaelices@gmail.com>
2016-12-14 00:13:31 +0100

    feat (login): Implemented a basic login form in the home screen.

--- loftsmartapp/src/pages/home/home.ts
+++ loftsmartapp/src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
 
 import { NavController } from 'ionic-angular';
 import { LoftsmartAPI } from '../../providers/api'
@@ -8,14 +9,27 @@ import { LoftsmartAPI } from '../../providers/api'
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public user: any;
 
-  constructor(public navCtrl: NavController, public api: LoftsmartAPI) {
+  constructor(public navCtrl: NavController,
+              public toastCtrl: ToastController,
+              public api: LoftsmartAPI) {
+    this.user = {
+      username: 'johnsmith@smith.com',
+      password: 'pepe1234'
+    };
   }
 
   login() {
-    this.api.login('johnsmith@smith.com', 'pepe1234').then(
+    this.api.login(this.user.username, this.user.password).then(
       () => {
         console.log('autenticado');
+        let toast = this.toastCtrl.create({
+          message: 'Welcome',
+          duration: 3000,
+          position: 'top'
+        });
+        toast.present();
       }, (err) => {
         console.log('error');
       });
